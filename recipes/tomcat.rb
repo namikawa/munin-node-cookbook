6f0f6ca4df27a5fda6ecb701cc7f70e6f39092b9
namikawa <rx7.fd3s.rs@gmail.com>
2014-07-16 16:59:45 +0900

    support tomcat

---
+++ recipes/tomcat.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook Name:: munin-node
+# Recipe:: tomcat
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "munin-node::default"
+
+package "perl-XML-Simple" do
+  action :install
+end
+
+%w{ 
+  java_fd
+  jstat_gccount
+  jstat_gctime
+  jstat_heap
+  tomcat_access
+  tomcat_jvm
+  tomcat_threads
+}.each do |script|
+  munin_node_plugin script do
+    script_name script
+    action [ :install, :enable ]
+  end 
+end
+
