76fbf79598ce6327cb5d711c68fa3219eb8e3d4f
namikawa <rx7.fd3s.rs@gmail.com>
2014-07-28 17:05:57 +0900

    add recipe for nginx

---
+++ recipes/nginx.rb
@@ -0,0 +1,37 @@
+#
+# Cookbook Name:: munin-node
+# Recipe:: nginx
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "munin-node::default"
+
+%w{
+  nginx_combined_
+  nginx_memory
+}.each do |script|
+  munin_node_plugin script do
+    script_name script
+    action :install
+  end
+end
+
+%w{
+  nginx_memory
+  nginx_status
+  nginx_request
+}.each do |script|
+  munin_node_plugin script do
+    script_name script
+    action :enable
+  end
+end
+
+munin_node_plugin "nginx_combined_127.0.0.1" do
+  script_name "nginx_combined_"
+  action :enable
+end
+
